7d68c6ef82a8bdf5524e61dccc7a4e6d9e08e201
VoVietBac <bac24062002@gmail.com>
2024-10-23 16:24:29 +0700

    ?

---
+++ Server4/src/main/java/org/app/Groups.java
@@ -0,0 +1,15 @@
+package org.app;
+
+import java.util.List;
+
+public class Groups {
+
+	private int groupID;
+
+	private List<Integer> admin;
+
+	private String groupName;
+
+	private List<Schedules> currentSchedules;
+
+}
---
+++ Server4/src/main/java/org/app/Schedules.java
@@ -0,0 +1,13 @@
+package org.app;
+
+import java.sql.Timestamp;
+
+public class Schedules {
+
+	private int scheduleID;
+
+	private Timestamp timeSet;
+
+	private boolean isRecored;
+
+}
