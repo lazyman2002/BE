9b6ef9e4eeb9029eecc6ebc0e498d9b2a8858272
VoVietBac <bac24062002@gmail.com>
2024-10-23 17:42:56 +0700

    ?

--- Server4/src/main/java/org/app/Groups.java
+++ Server4/src/main/java/org/app/Groups.java
@@ -1,15 +1,70 @@
 package org.app;
 
 import java.util.List;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Groups {
-
 	private int groupID;
-
-	private List<Integer> admin;
-
+	private ConcurrentHashMap<Integer, Boolean> admin;
 	private String groupName;
+	private ConcurrentHashMap<Schedules, Boolean> currentSchedules;
 
-	private List<Schedules> currentSchedules;
+	private static int currentID;
 
+	public Groups(String groupName, int admin) {
+		this.groupName = groupName;
+		this.groupID = currentID;
+		currentID+=1;
+		this.admin = new ConcurrentHashMap<>();
+		this.admin.put(admin, true);
+		this.currentSchedules = new ConcurrentHashMap<>();
+	}
+
+	public int getGroupID() {
+		return groupID;
+	}
+	public ConcurrentHashMap<Integer, Boolean> getAdmin() {
+		return admin;
+	}
+	public String getGroupName() {
+		return groupName;
+	}
+	public ConcurrentHashMap<Schedules, Boolean> getCurrentSchedules() {
+		return currentSchedules;
+	}
+
+	public void setAdmin(ConcurrentHashMap<Integer, Boolean> admin) {
+		this.admin = admin;
+	}
+	public void setGroupName(String groupName) {
+		this.groupName = groupName;
+	}
+	public void setCurrentSchedules(ConcurrentHashMap<Schedules, Boolean> currentSchedules) {
+		this.currentSchedules = currentSchedules;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Groups groups = (Groups) o;
+		return groupID == groups.groupID && Objects.equals(admin, groups.admin) && Objects.equals(groupName, groups.groupName) && Objects.equals(currentSchedules, groups.currentSchedules);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(groupID, admin, groupName, currentSchedules);
+	}
+
+	@Override
+	public String toString() {
+		final StringBuffer sb = new StringBuffer("Groups{");
+		sb.append("groupID=").append(groupID);
+		sb.append(", admin=").append(admin);
+		sb.append(", groupName='").append(groupName).append('\'');
+		sb.append(", currentSchedules=").append(currentSchedules);
+		sb.append('}');
+		return sb.toString();
+	}
 }
--- Server4/src/main/java/org/app/Schedules.java
+++ Server4/src/main/java/org/app/Schedules.java
@@ -1,13 +1,42 @@
 package org.app;
 
 import java.sql.Timestamp;
+import java.util.Objects;
 
 public class Schedules {
-
 	private int scheduleID;
-
 	private Timestamp timeSet;
-
 	private boolean isRecored;
 
+	private static int currentID = 1;
+
+	public Schedules(Timestamp timeSet, boolean isRecored) {
+		this.timeSet = timeSet;
+		this.isRecored = isRecored;
+		this.scheduleID = currentID;
+		currentID+=1;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		Schedules schedules = (Schedules) o;
+		return scheduleID == schedules.scheduleID && isRecored == schedules.isRecored && Objects.equals(timeSet, schedules.timeSet);
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(scheduleID, timeSet, isRecored);
+	}
+
+	@Override
+	public String toString() {
+		final StringBuffer sb = new StringBuffer("Schedules{");
+		sb.append("scheduleID=").append(scheduleID);
+		sb.append(", timeSet=").append(timeSet);
+		sb.append(", isRecored=").append(isRecored);
+		sb.append('}');
+		return sb.toString();
+	}
 }
